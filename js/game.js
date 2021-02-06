bbfed106e28edab49dd0ae994f60e39f87d72324
Joshua <40190305@esmad.ipp.pt>
2021-02-06 23:16:55 +0000

    Adding style to table

---
+++ js/game.js
@@ -0,0 +1,11 @@
+//variavéis para players
+let playerOne;
+let playerTwo;
+
+while (!playerOne) {
+    playerOne = prompt('Jogador 1 : Digite o seu Nome');
+}
+
+while (!playerTwo) {
+    playerTwo = prompt('Jogador 2 : Digite o teu nome')
+}

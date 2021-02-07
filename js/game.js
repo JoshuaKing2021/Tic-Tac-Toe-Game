756bcd65a3a7651399e3663dfabffb8e43f2ae16
Joshua <40190305@esmad.ipp.pt>
2021-02-07 00:52:03 +0000

    CSS updates

--- js/game.js
+++ js/game.js
@@ -3,9 +3,16 @@ let playerOne;
 let playerTwo;
 
 while (!playerOne) {
-    playerOne = prompt('Jogador 1 : Digite o seu Nome');
+    playerOne = prompt('Jogador 1 : Digite o teu nome')
 }
 
 while (!playerTwo) {
     playerTwo = prompt('Jogador 2 : Digite o teu nome')
 }
+
+//variavéis com elementos HTML 
+let playerTurn = document.querySelector('.playerTurn');
+let playerTurn2 = document.querySelector('.playerTurnTwo');
+
+playerTurn.textContent = `${playerOne} é a tua Vez`;
+playerTurn2.textContent = `${playerTwo} é a tua Vez`
